412e0a60c5ebed0b3bd733a44e68751165755fd2
fahad afailkawi <143465951+alfailkawi@users.noreply.github.com>
2023-09-05 02:49:45 +0300

    cw5

---
+++ script.js
@@ -0,0 +1,35 @@
+alert ('مرحبا بك في موقعي , أنا فهد وسوف احسب درجتك')
+
+let grade = prompt("قم بادخال درجتك")
+alert(grade);
+
+console.log("%c grade", "color:blue;");
+
+if (grade<= 100 && grade>=90 ){
+console.log("لقد حصلت على امتياز")
+
+}
+else if (grade<=89 && grade>=80  ){
+    console.log(" لقد حصلت على جيد جداَ")
+
+    }
+
+    else if (grade<=79 && grade>=70  ){
+        console.log(" لقد حصلت على جيد ")
+
+        }
+
+        else if (grade<=69 && grade>=60  ){
+            console.log("لقد حصلت على مقبول ")
+
+            }
+
+            else if (grade<=59 && grade>=50  ){
+                console.log("لقد حصلت على ضعيف ")
+
+                }
+
+                else if (grade<50  ){
+                    console.log("راسب")
+                }
+                // جافا سكربت ليست جافا
